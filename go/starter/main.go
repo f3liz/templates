181b7957a3badc77060fb4b7ac89155b4394df26
arcjun01 <arciaga.juno@student.greenriver.edu>
2024-12-02 00:15:57 +0000

    Tested a command in main within Go directory

--- go/starter/main.go
+++ go/starter/main.go
@@ -6,25 +6,7 @@ import (
 
 	"github.com/appwrite/sdk-for-go/appwrite"
 	"github.com/open-runtimes/types-for-go/v4/openruntimes"
-
-	"github.com/appwrite/sdk-for-go/appwrite"
-	"github.com/appwrite/sdk-for-go/client"
-	"github.com/appwrite/sdk-for-go/databases"
-	"github.com/appwrite/sdk-for-go/models"
 )
-
-var (
-	appwriteClient    client.Client
-	todoDatabase      *models.Database
-	todoCollection    *models.Collection
-	appwriteDatabases *databases.Databases
-)
-
-func main() {
-	appwriteClient = appwrite.NewClient(
-		appwrite.WithProject("67462c7b000e193b99aa"),
-		appwrite.WithKey("standard_17e9b2c441aa0f15dc1f186f0752a5555364196501843194a2e0ec43fa2c09f21bee4bde69919f0850ac935d2872bb2da8f6bb79412d1e62e46030a14c306f9c43bcae27985b36683732e3e1ef42e7ed1a1e25daaf3d03e2ea225cbc65987d7c7939934126ff8f48a564dc4c4130e21a34569dff04256f85246befde44f252ab"),
-	)
 
 type Response struct {
 	Motto       string `json:"motto"`
@@ -67,42 +49,3 @@ func Main(Context openruntimes.Context) openruntimes.Response {
 		GetInspired: "https://builtwith.appwrite.io",
 	})
 }
-
-func prepareDatabase() {
-	appwriteDatabases = appwrite.NewDatabases(appwriteClient)
-
-	todoDatabase, _ = appwriteDatabases.Create(
-		id.Unique(),
-		"TodosDB",
-	)
-
-	todoCollection, _ = appwriteDatabases.CreateCollection(
-		todoDatabase.Id,
-		id.Unique(),
-		"Todos",
-	)
-
-	appwriteDatabases.CreateStringAttribute(
-		todoDatabase.Id,
-		todoCollection.Id,
-		"title",
-		255,
-		true,
-	)
-
-	appwriteDatabases.CreateStringAttribute(
-		todoDatabase.Id,
-		todoCollection.Id,
-		"description",
-		255,
-		false,
-	)
-
-	appwriteDatabases.CreateBooleanAttribute(
-		todoDatabase.Id,
-		todoCollection.Id,
-		"isComplete",
-		true,
-	)
-}
-
